09a90f2e896ae05d79dbd32c7c7acfea0c6448e1
kelly <kelly.olida.pro@gmail.com>
2021-11-23 21:59:03 +0100

    Create App.jsx / clean / add Header component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header name="Jenny" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export const Header = ({name}) => {
+  return (
+      <div className='headder-div'>Welcome {name}</div>
+  );
+}
