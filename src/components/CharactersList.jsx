ed92c3242eb15ac0abfa69010293ea8aaf45d6cb
kelly <kelly.olida.pro@gmail.com>
2021-11-24 00:19:26 +0100

    add filtering on characters Alive or All characters

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -4,19 +4,41 @@ import axios from 'axios';
 
 export const CharactersList = () => {
   const [characters, setCharacters] = useState([]);
+  const [characterAliveClick, setCharacterAliveclick] = useState(false);
   
   useEffect(() => {
-      axios.get('https://rickandmortyapi.com/api/character')
-      .then(response => setCharacters(response.data.results))
+      getCharactersFromApi();
   }, []);
+
+  const getCharactersFromApi = () => {
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => setCharacters(response.data.results))
+  }
+
+  
+
+  const filterCharacters = () => {
+    if(characterAliveClick === false){
+      setCharacterAliveclick(true)
+      setCharacters(characters.filter(character => character.status === 'Alive'))
+    }
+    else{
+      setCharacterAliveclick(false);
+      getCharactersFromApi();
+    }
+    
+  }
 
   console.log(characters)
 
 
   return (
       <div className='charactersList-div'>
+        <div>
+          <button className='button-filter' type="button" onClick={() => filterCharacters()} >{characterAliveClick ? "Get All Characters":"Get Alive Characters"}</button>
+        </div>
         {
-          characters ? characters.map(character => <CharacterCard key={character.id} character={character}/>) :
+          characters ? characters.map(character => <CharacterCard key={character.id} character={character} />) :
           'loading...'
         }
       </div>
